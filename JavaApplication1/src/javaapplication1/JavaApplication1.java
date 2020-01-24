5d3c2ce16fbdf1161b538f11b4f19c0245992136
kaur7120 <59750243+kaur7120@users.noreply.github.com>
2020-01-24 16:14:15 -0500

    Update JavaApplication1.java

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,4 +1,4 @@
-/*
+/*https://github.com/kaur7120/javaApplication1/pulls
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -16,6 +16,8 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello");
+        System.out.println("how are you");
     }
     
 }
